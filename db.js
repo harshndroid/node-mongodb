edd2cfd464107f2560ba0ef6e8ae5c5733414877
harsh-chalo <52203103+harshndroid@users.noreply.github.com>
2024-04-02 23:46:54 +0530

    fix

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect('mongodb://0.0.0.0:27017/');
+mongoose
+  .connect('mongodb://0.0.0.0:27017/')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => {
+    console.log('Connected failed!', err);
+  });
 // mongoose.connect(process.env.MONGO_DB_URL);
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('connected', () => console.log('Connected to database!'));
-db.on('disconnected', () => console.log('Databse Disconnected!'));
-db.on('error', (err) => console.log('Connected failed!', err));
+// db.on('connected', () => console.log('Connected to database!'));
+// db.on('disconnected', () => console.log('Databse Disconnected!'));
+// db.on('error', (err) => console.log('Connected failed!', err));
