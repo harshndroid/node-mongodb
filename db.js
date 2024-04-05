bb87ac75a7440789a2df3cd3ef22ff311c4e23d3
harsh-chalo <52203103+harshndroid@users.noreply.github.com>
2024-04-06 00:03:24 +0530

    from env

--- db.js
+++ db.js
@@ -2,20 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose
-  // .connect('mongodb://localhost:27017', { family: 4 })
-  .connect(
-    'mongodb+srv://harshsuman0802:node-mongodb123@node-mongodb.v5kghef.mongodb.net/'
-  )
+  // .connect('mongodb://localhost:27017',)
+  .connect(process.env.MONGO_DB_URL)
   .then(() => {
     console.log('Connected to database!');
   })
   .catch((err) => {
     console.log('Connected failed!', err);
   });
-// mongoose.connect(process.env.MONGO_DB_URL);
-
-// const db = mongoose.connection;
-
-// db.on('connected', () => console.log('Connected to database!'));
-// db.on('disconnected', () => console.log('Databse Disconnected!'));
-// db.on('error', (err) => console.log('Connected failed!', err));
