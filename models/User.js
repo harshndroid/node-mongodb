10bacf5fef8fe74f92ae0acd050780aa9dc77412
harsh-chalo <52203103+harshndroid@users.noreply.github.com>
2024-04-11 12:11:52 +0530

    added photourl in schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   phone: { type: Number, required: true, unique: true },
   location: { type: Object },
   lastSeenAt: { type: Number },
+  photoUrl: { type: String },
 });
 
 const User = mongoose.model('User', userSchema);
