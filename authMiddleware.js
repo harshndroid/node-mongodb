d88c18354a534f197160d1466efbc200857cab6a
harsh-chalo <52203103+harshndroid@users.noreply.github.com>
2024-04-11 12:12:14 +0530

    first decode token then find user by id

--- authMiddleware.js
+++ authMiddleware.js
@@ -3,23 +3,21 @@ const User = require('./models/User');
 
 const authMiddleware = async function (req, res, next) {
   const token = req.headers.authorization.split(' ')[1];
-  const id = req.body.id; // mongoose id
-  const isUserPresent = await User.findById(req.body.id);
-  console.log('=======isUserPresent', isUserPresent);
-  if (isUserPresent) {
-    try {
-      const decoded = jwt.verify(token, id);
-      console.log('======jwt decoded success', decoded);
-      req.jwtDecodedValue = decoded;
-    } catch (err) {
-      console.log('======jwt decoded error', err);
-      req.jwtDecodedValue = '';
-    } finally {
-      next();
+  try {
+    const decoded = jwt.verify(token, 'somerandomtext');
+    console.log('======jwt decoded success', decoded);
+    req.jwtDecodedValue = decoded;
+    const isUserPresent = await User.findById(decoded.id);
+    console.log('=======isUserPresent', isUserPresent.phone);
+    if (isUserPresent) next();
+    else {
+      // dont call next()
+      res.status(404).json({ data: `User ${id} does not exist!` });
     }
-  } else {
-    // dont call next()
-    res.status(404).json({ data: `User ${id} does not exist!` });
+  } catch (err) {
+    console.log('======jwt decoded error', err);
+    req.jwtDecodedValue = '';
+    next();
   }
 };
 
